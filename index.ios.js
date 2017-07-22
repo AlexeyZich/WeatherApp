d520b9a145eba0b38bf4236977c95cc5afb14e95
AlexeyZich <zich.alexey@gmail.com>
2017-07-22 18:34:11 +0500

    Get geolocation and show country, city, temperature

---
+++ index.ios.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import { 
+  StyleSheet, 
+  Text, 
+  View,
+  AppRegistry,
+  ActivityIndicator,
+} from 'react-native';
+
+export default class WeaterApp extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      lat: null,
+      lng: null,
+      isLoading: true,
+      enterString: '',
+      country: '',
+      city: '',
+      gradeC: null,
+    }
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
+      },
+      (error) => alert(JSON.stringify(error)),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+    );
+  }
+
+  componentDidUpdate(prevState) {
+    if(prevState.lat !== null && prevState.lng !== nullх) {
+      this.fetchWeatherJSON();
+    }
+  }
+
+  fetchWeatherJSON() {
+    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${this.state.lat}&lon=${this.state.lng}&APPID=`    
+    console.log(url);
+    fetch(url)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log(responseJson);
+
+        this.setState({
+          isLoading: false,
+          city: responseJson.name,
+          country: responseJson.sys.country,
+          gradeC: Math.round(responseJson.main.temp - 273),
+        })
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+
+
+    // return 
+  // convertToCesium() {
+  //   return Math.round((responseJson.main.temp - 273) * 9 / 5 + 32)
+  // }
+
+  render() {
+    return (
+      <View>
+        <View style={styles.topHeader}>
+          <Text style={styles.textStyle}>Погода</Text>
+          <Text>{this.state.country}</Text>
+          <Text>{this.state.city}</Text>
+          <Text>{this.state.gradeC}</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+        // <View style={styles.topHeader}>
+        //   <Text style={styles.textStyle}>Погода</Text>
+        //   <Text>{this.state.city}</Text>
+        //   <Text>{this.state.gradeC}</Text>
+        // </View>
+
+
+const styles = {
+  // container: {
+  //   flex: 1,
+  //   justifyContent: 'center',
+  //   alignItems: 'center',
+  //   backgroundColor: '#F5FCFF',
+  // },
+  topHeader: {
+    flex: 1,
+    marginTop: 40,
+    alignItems: 'center',
+  },
+  textStyle: {
+    fontSize: 44,    
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+};
+
+AppRegistry.registerComponent('WeaterApp', () => WeaterApp);
